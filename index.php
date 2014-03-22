399a5fa75dba6557c8a68bafa459723b0adaef94
Matt Oehrlein <matt.oehrlein@gmail.com>
2014-03-22 21:48:12 +0000

    adds embedded mailchimp signup form

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<title>MattOehrlein.com</title>
 		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+		<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" type="text/css" href="./stylesheets/style.css"/>
 		<link rel="stylesheet" type="text/css" href="./stylesheets/jquery.pageslide.css"/>
 		<!-- Fonts ================================================== -->
@@ -113,14 +114,43 @@
 			<div id="contact-container">
 				<div id="contact-title">
 					<h2>Contact:</h2>
-					</div>
-					<ul id="contact-list">
+				</div>
+				<ul id="contact-list">
 					<li><a href="mailto:matt.oehrlein@gmail.com">matt.oehrlein@gmail.com</a></li>
 					<li><a href="http://www.linkedin.com/in/mattoehrlein">www.linkedin.com/in/mattoehrlein</a></li>
 					<li><a href="http://www.facebook.com/matt.oehrlein">facebook.com/matt.oehrlein</a></li>
 				</ul>
 			</div> <!-- end contact info -->
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup_title">
+				<h2>Get Biannual Updates From Me:</h2>
+			</div>
+			<div id="mc_embed_signup">
+			<form action="http://mattoehrlein.us3.list-manage2.com/subscribe/post?u=1c967500eb043a5b894c74a30&amp;id=dcbd5a2432" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				
+			<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+			<div class="mc-field-group">
+				<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+			</label>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+			<div class="mc-field-group">
+				<label for="mce-FNAME">First Name </label>
+				<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+			</div>
+			<div class="mc-field-group">
+				<label for="mce-LNAME">Last Name </label>
+				<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+			</div>
+				<div id="mce-responses" class="clear">
+					<div class="response" id="mce-error-response" style="display:none"></div>
+					<div class="response" id="mce-success-response" style="display:none"></div>
+				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			    <div style="position: absolute; left: -5000px;"><input type="text" name="b_1c967500eb043a5b894c74a30_dcbd5a2432" value=""></div>
+				<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			</form>
+			</div>
+		<!--End mc_embed_signup-->
 		</div> <!-- end content container -->
-
 	</body>
 </html>
